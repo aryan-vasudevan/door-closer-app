7259f16d0bca576107f9eaa7c71bc2d40b37a7cd
aryan-vasudevan <vasudevan.aryan@gmail.com>
2025-08-13 09:41:55 -0400

    Initial Commit

---
+++ app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app
+//
+//  Created by Aryan Vasudevan on 2025-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Aryan Vasudevan on 2025-08-13.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
